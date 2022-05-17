2c4e19edbdbd60b092b00371e336215973973f68
Bradley Wayne Arnold <82877822+B1r2a3d4l5y@users.noreply.github.com>
2022-05-17 09:55:38 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
-    $("#button").click(function(){
-        $(".text2").animate({opacity:1});
-        $(".text3").animate({opacity:1})
-    })
+$("#button").click(function(){
+    $(".text2").animate({opacity:1});
+})
 })
